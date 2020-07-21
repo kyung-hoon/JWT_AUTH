547efb0238000626bdbfcd818cb0a1f6722e7e25
kyunghoon_min <kyunghoon_min@tmax.co.kr>
2020-07-21 16:19:58 +0900

    index.js create api 구현

--- index.js
+++ index.js
@@ -5,6 +5,46 @@ import path from 'path';
 
 const app =  express();
 const port = 8080;
-app.use(express.static(path.join(os.homedir,'indexPage','target')));
+app.use(express.static(path.join(os.homedir(),'indexPage','target')));
 app.listen(port);
+console.log('express listening on port : 8080');
 
+
+app.use(express.json());
+
+app.post('/create',(req,res) =>{
+    const name = req.body.workspaceName;
+    const dirLocation = path.join(os.homedir(),name);
+    fs.mkdirSync(dirLocation);
+    const lastModified = fs.statSync(dirLocation).mtime.toISOString().replace(/T/, ' '). replace(/\..+/, '');
+    if(!fs.existsSync(path.join(os.homedir(),'workspace.json'))) {
+        let workSpaceArray = new Array();
+        const workSpaceObejct ={
+            name: name,
+            location: dirLocation,
+            lastModified : lastModified
+        }
+        workSpaceArray.push(workSpaceObejct);
+        const content = {
+            "workspaces" : workSpaceArray
+        }
+        fs.writeFileSync(path.join(os.homedir(),'workspace.json'),JSON.stringify(content));
+        res.send(content);
+    }else{
+        const content = fs.readFileSync(path.join(os.homedir(),'workspace.json'),JSON.stringify(content));
+        let workSpaceArray = JSON.parse(content).workspaces;
+        const workSpaceObejct ={
+            name: name,
+            location: dirLocation,
+            lastModified : lastModified
+        }
+        workSpaceArray.push(workSpaceObejct);
+        const updatedContent = {
+            "workspaces": workSpaceArray 
+        }
+        console.log(updatedContent);
+        fs.writeFileSync(path.join(os.homedir(),'workspace.json'),JSON.stringify(updatedContent));
+        res.send(updatedContent);
+    }
+    
+});
