0cc9457ba268cb549bc9b98b02fae0ccf4c42b37
kyunghoon_min <kyunghoon_min@tmax.co.kr>
2020-07-21 17:28:00 +0900

    create 요청 방어 코드 구현 및 get 요청 구현

--- index.js
+++ index.js
@@ -2,66 +2,88 @@ import * as fs from 'fs-extra';
 import express from 'express';
 import os from 'os';
 import path from 'path';
+import cors from 'cors';
 
-const app =  express();
+const app = express();
 const port = 8080;
-app.use(express.static(path.join(os.homedir(),'indexPage','target')));
+app.use(cors());
+app.use(express.static(path.join(os.homedir(), 'indexPage', 'target')));
 app.listen(port);
 console.log('express listening on port : 8080');
 
 
 app.use(express.json());
 
-app.post('/create',(req,res) =>{
+app.post('/create', (req, res) => {
     const name = req.body.workspaceName;
-    const dirLocation = path.join(os.homedir(),name);
-    fs.mkdirSync(dirLocation);
-    const lastModified = fs.statSync(dirLocation).mtime.toISOString().replace(/T/, ' '). replace(/\..+/, '');
-    if(!fs.existsSync(path.join(os.homedir(),'workspace.json'))) {
-        let workSpaceArray = new Array();
-        const workSpaceObejct ={
-            name: name,
-            location: dirLocation,
-            lastModified : lastModified
+    const dirLocation = path.join(os.homedir(), 'TopWebStudio', name);
+    if (!fs.existsSync(dirLocation)) {
+        fs.mkdirSync(dirLocation, { recursive: true });
+        const lastModified = fs.statSync(dirLocation).mtime.toLocaleString("ko-KR", { timeZone: "Asia/Seoul" }).replace(/T/, ' ').replace(/\..+/, '');
+        if (!fs.existsSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'))) {
+            let workSpaceArray = new Array();
+            const workSpaceObejct = {
+                name: name,
+                location: dirLocation,
+                lastModified: lastModified
+            }
+            workSpaceArray.push(workSpaceObejct);
+            const content = {
+                "workspaces": workSpaceArray
+            }
+            fs.writeFileSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'), JSON.stringify(content));
+            res.send(content);
+        } else {
+            const content = fs.readFileSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'));
+            let workSpaceArray = JSON.parse(content).workspaces;
+            const workSpaceObejct = {
+                name: name,
+                location: dirLocation,
+                lastModified: lastModified
+            }
+            workSpaceArray.push(workSpaceObejct);
+            const updatedContent = {
+                "workspaces": workSpaceArray
+            }
+            console.log(updatedContent);
+            fs.writeFileSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'), JSON.stringify(updatedContent));
+            res.send(updatedContent);
         }
-        workSpaceArray.push(workSpaceObejct);
-        const content = {
-            "workspaces" : workSpaceArray
-        }
-        fs.writeFileSync(path.join(os.homedir(),'workspace.json'),JSON.stringify(content));
-        res.send(content);
-    }else{
-        const content = fs.readFileSync(path.join(os.homedir(),'workspace.json'));
-        let workSpaceArray = JSON.parse(content).workspaces;
-        const workSpaceObejct ={
-            name: name,
-            location: dirLocation,
-            lastModified : lastModified
-        }
-        workSpaceArray.push(workSpaceObejct);
-        const updatedContent = {
-            "workspaces": workSpaceArray 
-        }
-        console.log(updatedContent);
-        fs.writeFileSync(path.join(os.homedir(),'workspace.json'),JSON.stringify(updatedContent));
-        res.send(updatedContent);
+
+    } else {
+        res.status(500).send('workspace already exists');
     }
-    
+
 });
 
-app.post('/remove',(req, res)=>{
+app.post('/remove', (req, res) => {
     const name = req.body.workspaceName;
-    const dirLocation = path.join(os.homedir(),name);
-    fs.rmdirSync(dirLocation,{recursive: true});
-    const content = fs.readFileSync(path.join(os.homedir(),'workspace.json'));
+    const dirLocation = path.join(os.homedir(), 'TopWebStudio', name);
+    fs.rmdirSync(dirLocation, { recursive: true });
+    const content = fs.readFileSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'));
     let workSpaceArray = JSON.parse(content).workspaces;
-    const index = workSpaceArray.map(function(item){
+    const index = workSpaceArray.map(function (item) {
         return item.name;
     }).indexOf(name);
-    workSpaceArray.splice(index,1);
+    workSpaceArray.splice(index, 1);
     const updatedContent = {
         workspaces: workSpaceArray
     }
-    fs.writeFileSync(path.join(os.homedir(),'workspace.json'),JSON.stringify(updatedContent));
+    fs.writeFileSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'), JSON.stringify(updatedContent));
     res.send(updatedContent);
 });
+
+
+app.get('/getWorkspaces', (req, res) => {
+    if (!path.join(os.homedir(), 'TopWebStudio', 'workspace.json')){
+        const workSpaceArray = new Array();
+        const content = {
+            workspaces : workSpaceArray
+        }
+        fs.writeFileSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'), JSON.stringify(content));
+        res.send(content);
+    }else{
+        const content = fs.readFileSync(path.join(os.homedir(), 'TopWebStudio', 'workspace.json'), JSON.stringify(content));
+        res.send(content);
+    }
+})
